3b92aa6890c50424e98550984f71cce5ad2d3078
terzogenito <otineg@gmail.com>
2024-12-25 17:29:46 +0700

    add bootstrap, page styling, add 404

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.shortcuts import render
 
 urlpatterns = [
     path('', views.main_view, name='main'),
 ]
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = 'masj.urls.custom_404_view'
--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def main_view(request):
     return render(request, 'main.html')
