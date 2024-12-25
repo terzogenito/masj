52a7ce3d2936ebb1d7a1c00742907a3893529233
terzogenito <otineg@gmail.com>
2024-12-25 18:54:28 +0700

    add login

--- app/views.py
+++ app/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def main_view(request):
-    return render(request, 'main.html')
+    if request.user.is_authenticated:
+        users = User.objects.select_related('profile').all()
+        return render(request, 'main.html', {'users': users})
+    else:
+        return redirect('login')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'login.html')
