e1cb70116f9366ae01b1681d0f681f4ded3cc39c
terzogenito <otineg@gmail.com>
2024-12-25 14:52:44 +0700

    create admin model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    def __str__(self):
+        return self.username
