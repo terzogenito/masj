9b60468a447508d5e6eec5086a8a55387f755a47
terzogenito <otineg@gmail.com>
2024-12-25 17:32:02 +0700

    add logging

--- app/views.py
+++ app/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.contrib import messages
+from django.shortcuts import render
 
 def main_view(request):
     return render(request, 'main.html')
--- masj/settings.py
+++ masj/settings.py
@@ -82,3 +82,22 @@ STATIC_URL = '/static/'
 STATIC_ROOT = "static"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'error.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
